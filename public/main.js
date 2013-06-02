2225ac875defbc36fde89c9007d6176465040695
ohnorobo <sar.laplante@gmail.com>
2013-06-02 12:35:31 +0800

    getting a single point to add from the db

--- public/main.js
+++ public/main.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 	});
 
 
+  //Geolocation
   GMaps.geolocate({
     success: function(position) {
            map.setCenter(position.coords.latitude, position.coords.longitude);
@@ -18,6 +19,100 @@ $(document).ready(function() {
                },
   });
 
+
+
+  //Backbone
+  console.log('hi'); 
+
+  var Pantry  = Backbone.Model.extend({
+    defaults: {
+      id: "0",
+      source: "GBFB",
+      website: "www.something.com",
+      email: "someone@something.com",
+      site_name: "Some Pantry",
+      address: "123 Street",
+      city: "Boston",
+      phone: "123-4567",
+      zipcode: "12345",
+      hours: [{}] //unclear data rep as of yet
+      },
+      initialize: function() {
+        console.log('pantry init');
+      }
+    });
+
+  var Pantries = Backbone.Collection.extend({
+    defaults: {
+      model: Pantry
+    },
+    model: Pantry,
+    url: '/search',
+    initialize: function() {
+      console.log('all pantries init');
+    }
+  });
+
+  var MyView = Backbone.View.extend({
+    initialize: function() {
+        _.bindAll(this,"render");
+        console.log('view init');
+
+        collection.bind('reset', this.render, this);
+        collection.fetch(
+          { success: function() {
+            // fetch successfully completed
+            console.log("post-fetch");
+            console.log(collection.toJSON());
+            collection.each(
+              function(item, i) {console.log("lol"); addPantryToMap(item); } );
+            }
+          } 
+          ).then( this.render() );
+        console.log(collection.toJSON())
+        console.log("finished init");
+        this.render();
+     },
+
+    render: function() {
+      var self = this;
+      console.log('Render called.');
+      console.log(collection.toJSON());
+      collection.each(
+        function(item, i) { console.log("lol"); addPantryToMap(item); }
+      );
+    }
+  });
+
+  collection = new Pantries();
+  var myView = new MyView(); //?
+
+  //Adding a Pantry
+  function addPantryToMap(pantry){
+    fullAddress = pantry.get("address") + " " + pantry.get("city") + " " + pantry.get("zipcode");
+    console.log("adding " + fullAddress);
+
+    GMaps.geocode({
+      address: fullAddress,
+      callback: function(results, status) {
+        if (status == 'OK') {
+            var latlng = results[0].geometry.location;
+                map.setCenter(latlng.lat(), latlng.lng());
+                  map.addMarker({
+                        title: 'title',
+                        lat: latlng.lat(),
+                        lng: latlng.lng(),
+                        infoWindow: {
+                          content: "<p> needs data </p>"
+                        }
+                      });
+                    }
+          }
+    });
+  }
+
+  //Examples
+  //TODO remove
   map.addMarker({
     lat: 42.3583,
     lng: -71.0603,
